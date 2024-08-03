dc0af318305282eada3339538a797f0b9c8b3677
Richard Heery <richardheery@gmail.com>
2024-08-03 22:15:19 +0200

    Updated rf_var_importance to take a dataset with response and features

---
+++ R/subset_selection.R
@@ -0,0 +1,56 @@
+#' Create row indices for K folds of a dataset
+#'
+#' @param data Dataset to split into folds
+#' @param K Number of folds. Default is 10. 
+#' @return A list with the row indices for K folds of data
+#' @export
+sample_k_fold_indices = function(data, K = 10){
+  
+  # Get a vector of row indices for data
+  indices = seq.int(nrow(data))
+  
+  # Create a vector with row indices for folds
+  indices_for_folds = split(sample(indices, length(indices)), rep(1:K, ceiling(length(indices)/K)))
+  return(indices_for_folds)
+  
+}
+
+#' Separate a dataset into a training and test set using specified folds of the data
+#'
+#' @param data Matrix or table to split into folds
+#' @param k_folds_indices A list with the indices for different folds of the data. 
+#' Each index should occur in only one fold. 
+#' @param test_fold Number of the fold from k_folds_indices which should be treated as the test set.
+#' @return A list with the training and test set splits of the data. 
+#' @export
+separate_training_and_test_data = function(data, k_folds_indices, test_fold){
+  
+  # Check that there are indices for each row in data
+  if(!all(sort(unlist(k_folds_indices)) == seq.int(nrow(data)))){
+    stop("Indices in k_folds_indices should cover all rows in data and each index sould occur in only a single fold")
+  }
+  
+  # Get a vector of row indices for data
+  test_indices = k_folds_indices[[test_fold]]
+  
+  # Separate data into training and test data
+  training_data = data[-test_indices, ]
+  test_data = data[test_indices, ]
+  
+  # Return a list with the training and test data sets
+  return(list(training = training_data, test = test_data))
+  
+}
+
+#' Separate a dataset into a training and test set using specified folds of the data
+#'
+#' @param data Matrix or table to split into folds
+#' @param response A vector with the response variable to be predicted.
+#' @param K Number of folds. Default is 10. 
+#' @return 
+#' @export
+cross_validated_subset_selection = function(data, response, K = 10){
+  
+  
+  
+}
--- R/trees.R
+++ R/trees.R
@@ -1,18 +1,23 @@
 #' Calculate variable importance using a random forest. 
 #'
-#' @param response A vector with the response variable to be predicted.
-#' @param features A matrix or data.frame of features where each column is a feature. 
+#' @param data Matrix or data.frame containing the response and features as columns.
+#' @param response Name of the column containing the response in data. 
 #' @param ntree Number of trees. Default is 500. 
 #' @param mtry Number of features used at each split. Uses same default as randomForest::randomForest(). 
 #' @return A matrix with the feature importance measures.
 #' @export
 rf_var_importance = function(data, response, mtry = NULL, ntree = 500){
   
+  # Convert data into a data.frame
+  data = as.matrix(data)
+  
   # Fit random forest to data
   if(is.null(mtry)){
-    rf = randomForest::randomForest(y = response, x = as.matrix(features), ntree = ntree, importance = TRUE)
+    rf = randomForest::randomForest(y = data[, response], 
+      x = data[, colnames(data) != response], ntree = ntree, importance = TRUE)
   } else {
-    rf = randomForest::randomForest(y = response, x = as.matrix(features), mtry = mtry, ntree = ntree, importance = TRUE)
+    rf = randomForest::randomForest(y = data[, response], 
+      x = data[, colnames(data) != response], mtry = mtry, ntree = ntree, importance = TRUE)
   }
   
   # Return feature importance from the model
